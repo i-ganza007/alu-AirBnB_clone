fb5c1c3f63afc8e110916091ea36533ec43873a2
Ian Ganza <144003894+i-ganza007@users.noreply.github.com>
2024-05-13 16:14:47 +0300

    Update base_model.py #10

--- models/base_model.py
+++ models/base_model.py
@@ -8,14 +8,14 @@ class BaseModel:
         self.id = str(uuid.uuid4())
         self.created_at = datetime.utcnow()
         self.updated_at = datetime.utcnow()
-    
+    # Dunder string method
     def __str__(self) -> str:
         return f'[{self.__class__.__name__}] ({self.id}) {self.__dict__}'
-    
+    # Updating method
     def save(self):
         self.updated_at = datetime.utcnow()
         return self.updated_at
-    
+    # Dict method
     def to_dict(self):
         my_dict = self.__dict__.copy()
         my_dict["updated_at"] = self.updated_at.isoformat()
