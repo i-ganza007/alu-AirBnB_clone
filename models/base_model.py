bbd6c151d734f0b23108c4ff8f8a2990abfc9ed0
Ian Ganza <144003894+i-ganza007@users.noreply.github.com>
2024-05-13 14:51:58 +0300

    Update base_model.py
    Added the __name__=="__main__" clause and added the python shebang

--- models/base_model.py
+++ models/base_model.py
@@ -4,18 +4,31 @@ import datetime
 class BaseModel:
     def __init__(self) -> None:
         self.id = str(uuid.uuid4())
-        self.created_at = datetime.datetime.now().isoformat()
-        self.updated_at = datetime.datetime.now().isoformat()
+        self.created_at = datetime.utcnow()
+        self.updated_at = datetime.utcnow()
 
     def __str__(self) -> str:
         return f'[{BaseModel.__name__}] ({self.id}) {self.__dict__}'
         
     def save(self):
-        self.updated_at = datetime.datetime.now().isoformat()
+        self.updated_at = datetime.utcnow()
         return self.updated_at
 
-# In the method below , We used the copy() method for the __dict__ so that we can avoid affecting the actual attributes .
+# In the method below, We used the copy() method for the __dict__ so that we can avoid affecting the actual attributes.
     def to_dict(self):
         MyDict = self.__dict__.copy()
         MyDict['__class__'] = BaseModel.__name__
         return MyDict
+
+if __name__ == "__main__":
+    my_model = BaseModel()
+    my_model.name = "My First Model"
+    my_model.my_number = 89
+    print(my_model)
+    my_model.save()
+    print(my_model)
+    my_model_json = my_model.to_dict()
+    print(my_model_json)
+    print("JSON of my_model:")
+    for key in my_model_json.keys():
+        print("\t{}: ({}) - {}".format(key, type(my_model_json[key]), my_model_json[key]))
