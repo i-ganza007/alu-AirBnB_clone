24c30927cf8c6103b44e7cb47e01c3ca0e011cef
Ian Ganza <144003894+i-ganza007@users.noreply.github.com>
2024-05-13 15:46:11 +0300

    Update base_model.py to PEP8 standards

--- models/base_model.py
+++ models/base_model.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 import uuid
-import datetime
+from datetime import datetime
+
 class BaseModel:
     def __init__(self) -> None:
         self.id = str(uuid.uuid4())
@@ -8,19 +9,19 @@ class BaseModel:
         self.updated_at = datetime.utcnow()
 
     def __str__(self) -> str:
-        return f'[{BaseModel.__name__}] ({self.id}) {self.__dict__}'
-        
+        return f'[{self.__class__.__name__}] ({self.id}) {self.__dict__}'
+
     def save(self):
         self.updated_at = datetime.utcnow()
         return self.updated_at
 
-# In the method below, We used the copy() method for the __dict__ so that we can avoid affecting the actual attributes.
+    # In the method below, we used the copy() method for the __dict__ so that we can avoid affecting the actual attributes.
     def to_dict(self):
-        MyDict = self.__dict__.copy()
-        MyDict["updated_at"] =  self.updated_at.isoformat()
-        MyDict["created_at"] =  self.created_at.isoformat()
-        MyDict['__class__'] = BaseModel.__name__
-        return MyDict
+        my_dict = self.__dict__.copy()
+        my_dict["updated_at"] = self.updated_at.isoformat()
+        my_dict["created_at"] = self.created_at.isoformat()
+        my_dict['__class__'] = self.__class__.__name__
+        return my_dict
 
 if __name__ == "__main__":
     my_model = BaseModel()
