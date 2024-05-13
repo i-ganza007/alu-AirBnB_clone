5b29dfda68324ca126616ce3f6bb3cbe2bb6342a
Ian Ganza <144003894+i-ganza007@users.noreply.github.com>
2024-05-13 15:49:46 +0300

    Update base_model.py to PEP8 #2

--- models/base_model.py
+++ models/base_model.py
@@ -3,6 +3,7 @@ import uuid
 from datetime import datetime
 
 class BaseModel:
+    
     def __init__(self) -> None:
         self.id = str(uuid.uuid4())
         self.created_at = datetime.utcnow()
