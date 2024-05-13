b71f1a8dffc167eae4bf662a7e245a28459589ab
Ian Ganza <144003894+i-ganza007@users.noreply.github.com>
2024-05-13 16:18:41 +0300

    Update base_model.py #11

--- models/base_model.py
+++ models/base_model.py
@@ -1,38 +1,75 @@
 #!/usr/bin/python3
+"""
+Module for the BaseModel class.
+"""
 import uuid
 from datetime import datetime
+import models
 
 
 class BaseModel:
-    def __init__(self) -> None:
+    def __init__(self, *args, **kwargs):
+        time_format = "%Y-%m-%dT%H:%M:%S.%f"
         self.id = str(uuid.uuid4())
         self.created_at = datetime.utcnow()
         self.updated_at = datetime.utcnow()
-    # Dunder string method
-    def __str__(self) -> str:
-        return f'[{self.__class__.__name__}] ({self.id}) {self.__dict__}'
-    # Updating method
+        
+        if kwargs:
+            for key, value in kwargs.items():
+                if key == "__class__":
+                    continue
+                elif key == "created_at" or key == "updated_at":
+                    setattr(self, key, datetime.strptime(value, time_format))
+                else:
+                    setattr(self, key, value)
+
+        models.storage.new(self)
+
     def save(self):
+        """
+
+        """
         self.updated_at = datetime.utcnow()
-        return self.updated_at
-    # Dict method
+        models.storage.save()
+
     def to_dict(self):
-        my_dict = self.__dict__.copy()
-        my_dict["updated_at"] = self.updated_at.isoformat()
-        my_dict["created_at"] = self.created_at.isoformat()
-        my_dict['__class__'] = self.__class__.__name__
-        return my_dict
+        """
+
+        """
+        inst_dict = self.__dict__.copy()
+        inst_dict["__class__"] = self.__class__.__name__
+        inst_dict["created_at"] = self.created_at.isoformat()
+        inst_dict["updated_at"] = self.updated_at.isoformat()
+
+        return inst_dict
+
+    def __str__(self):
+        """
+
+        """
+        class_name = self.__class__.__name__
+        return "[{}] ({}) {}".format(class_name, self.id, self.__dict__)
+
 
 if __name__ == "__main__":
     my_model = BaseModel()
-    my_model.name = "My First Model"
+    my_model.name = "My_First_Model"
     my_model.my_number = 89
+    print(my_model.id)
     print(my_model)
-    my_model.save()
-    print(my_model)
+    print(type(my_model.created_at))
+    print("--")
     my_model_json = my_model.to_dict()
     print(my_model_json)
     print("JSON of my_model:")
     for key in my_model_json.keys():
-        value = my_model_json[key]
-        print("\t{}: ({}) - {}".format(key, type(value), value))
+        print("\t{}: ({}) - {}".format(key, type(my_model_json[key]), my_model_json[key]))
+
+    print("--")
+    my_new_model = BaseModel(**my_model_json)
+    print(my_new_model.id)
+    print(my_new_model)
+    print(type(my_new_model.created_at))
+
+    print("--")
+    print(my_model is my_new_model)
